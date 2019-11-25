bc79a93bbaa42be9016921d20ae16605c085fdf0
James Hansberry <hansberryjam@gmail.com>
2019-11-25 14:41:11 -0800

    Users can only edit/delete their own profiles when logged in.  May need to look more into sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
+    reset_session
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
