afdb133a6288d221fc12947aa626d15dc1d777c4
limmingjun <limmingjun@berkeley.edu>
2019-12-04 16:39:51 -0800

    login on create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,7 @@ class UsersController < ApplicationController
         if @user.save
           format.html { redirect_to edit_user_path(@user), notice: 'User was successfully created.' }
           format.json { render :show, status: :created, location: @user }
+          session[:user_id] = @user.id
         else
           format.html { render :new }
           format.json { render json: @user.errors, status: :unprocessable_entity }
