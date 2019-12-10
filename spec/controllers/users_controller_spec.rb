77ee47d68d31113189034c0132a585225f3fba40
Umar Brown Momen <yungman69@airbears2-10-142-131-16.airbears2.1918.berkeley.edu>
2019-12-10 13:56:31 -0800

    its alive

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	before do
+		@usercount = User.count
+		@tutee = User.create!(email: "usertest@gmail.com",
+	      password_digest: "password",
+	      name: "Test User",
+	      major: "Film",
+	      philosophy: "Film is the best major, but I teach CS anyway",
+	      courses: "CS61A CS61B EE16A",
+	      about: "Gonna be a star one day")
+	end
+	it "create a user and make sure number of users goes up" do
+		User.count.should eq (@usercount + 1)
+	end
 
+	it "create a user and make sure its in the DB" do
+		@usercount = User.count
+		expect(User.where(email: "usertest@gmail.com", name: "Test User")).to exist
+	end
 end
